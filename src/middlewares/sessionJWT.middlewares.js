11d12100597d76e32190bb27c94deff7e82c8a7c
juliogayosso <j_chalo@hotmail.com>
2024-10-09 16:33:11 -0600

    send emails

---
+++ src/middlewares/sessionJWT.middlewares.js
@@ -0,0 +1,16 @@
+const jwt = require('jsonwebtoken')
+
+const sessionJWT = (req,res,next) => {
+
+    const user = req.userlogged
+
+    const token = jwt.sign(
+        { user },
+        process.env.TOKEN_SECRET,
+        {expiresIn: '1D'}
+    )
+    req.token = token
+    next()
+}
+
+module.exports = sessionJWT
